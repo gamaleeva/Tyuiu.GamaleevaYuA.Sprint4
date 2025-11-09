23d1742ae70aa2b4ada190fa9f39c22d7e1d522d
gamaleeva <gamaleevacin@mail.ru>
2025-11-10 02:24:30 +0500

    Task2

---
+++ Tyuiu.GamaleevaYuA.Sprint4.Task2.V3/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.GamaleevaYuA.Sprint4.Task2.V3.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #4 | Выполнила: Гамалеева Ю. А. | ИБКСб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномерные массивы (генератор случайных числел)                   *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #3                                                              *");
+        Console.WriteLine("* Выполнила: Гамалеева Юлия Андреевна | ИБКСб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 12 элементов заполненный         *");
+        Console.WriteLine("*  случайными значениями в диапазоне от 2 до 7 подсчитать сумму четных    *");
+        Console.WriteLine("* элементов массива.                                                      *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int len;
+        Console.WriteLine("Введите количество элементов массива: ");
+        len = Convert.ToInt32(Console.ReadLine());
+        int[] nums = new int[len];
+        Random rnd = new Random();
+        for (int i = 0; i < len; i++)
+        {
+            nums[i] = rnd.Next(2, 7);
+        }
+        Console.WriteLine();
+        Console.WriteLine("Массив: ");
+        for (int i = 0; i < nums.Length; i++)
+        {
+            Console.WriteLine(nums[i] + "\t");
+        }
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int res = ds.Calculate(nums);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
